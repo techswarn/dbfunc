84a3d60424172c7a9ecdb6261c998e3f2bb94fb4
techswarn <tech.swarns@gmail.com>
2022-07-08 15:30:26 +0530

    code update

--- packages/databasesCon/helloPg/hello.js
+++ packages/databasesCon/helloPg/hello.js
@@ -1,35 +1,44 @@
-// const { Client } = require('pg')
-// const dotenv = require('dotenv')
+// // const { Client } = require('pg')
+// // const dotenv = require('dotenv')
 
-// dotenv.config({path: './config.env'})
+// // dotenv.config({path: './config.env'})
+
+// function main(args) {
+// // console.log(process.env)
+//   // const client = new Client({
+//   //   host: process.env.PG_HOST,
+//   //   database: process.env.PG_DATABASE,
+//   //   port: process.env.PG_PORT,
+//   //   user: process.env.PG_USER,
+//   //   password: process.env.PG_PASSWORD,
+//   //   ssl: {
+//   //     rejectUnauthorized: false
+//   //   }
+//   // })
+
+//   // client.connect(err => {
+//   //   if (err) {
+//   //     console.error('connection error', err.stack)
+//   //   } else {
+//   //     console.log('connected')
+//   //   }
+//   // })
+  
+
+//    // let name = args.name || 'stranger'
+//    // let greeting = 'Hello ' + name + '!'
+//     //console.log(greeting)
+//     return {"body": "function called successfully"}
+//   }
+
+//   exports.main = main
+  
 
 function main(args) {
-// console.log(process.env)
-  // const client = new Client({
-  //   host: process.env.PG_HOST,
-  //   database: process.env.PG_DATABASE,
-  //   port: process.env.PG_PORT,
-  //   user: process.env.PG_USER,
-  //   password: process.env.PG_PASSWORD,
-  //   ssl: {
-  //     rejectUnauthorized: false
-  //   }
-  // })
+  let name = args.name || 'stranger'
+  let greeting = 'Hello ' + name + '!'
+  console.log(greeting)
+  return {"body": greeting}
+}
 
-  // client.connect(err => {
-  //   if (err) {
-  //     console.error('connection error', err.stack)
-  //   } else {
-  //     console.log('connected')
-  //   }
-  // })
-  
-
-   // let name = args.name || 'stranger'
-   // let greeting = 'Hello ' + name + '!'
-    //console.log(greeting)
-    return {"body": "function called successfully"}
-  }
-
-  exports.main = main
-  
+exports.main = main
