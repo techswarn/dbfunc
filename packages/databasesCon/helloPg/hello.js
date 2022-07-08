1c3f2968462f63c8b69015d31bcdb781fd113537
techswarn <tech.swarns@gmail.com>
2022-07-08 07:43:04 +0530

    code update

--- packages/databasesCon/helloPg/hello.js
+++ packages/databasesCon/helloPg/hello.js
@@ -1,4 +1,25 @@
+const { Client } = require('pg')
+
+
+
 function main(args) {
+
+  const client = new Client({
+    host: 'db-postgresql-do-user-11648032-0.b.db.ondigitalocean.com',
+    port: 25060,
+    user: 'doadmin',
+    password: 'AVNS_0POAcotmgRBoXxPuetJ',
+  })
+
+  client.connect(err => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected')
+    }
+  })
+  
+
     let name = args.name || 'stranger'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
