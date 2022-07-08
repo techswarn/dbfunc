6ee950363bef9991a4ca5f2288de114cc020bd40
techswarn <tech.swarns@gmail.com>
2022-07-08 15:01:20 +0530

    code update

--- packages/databasesCon/helloPg/hello.js
+++ packages/databasesCon/helloPg/hello.js
@@ -4,31 +4,32 @@ const dotenv = require('dotenv')
 dotenv.config({path: './config.env'})
 
 function main(args) {
+console.log(process.env)
+  // const client = new Client({
+  //   host: process.env.PG_HOST,
+  //   database: process.env.PG_DATABASE,
+  //   port: process.env.PG_PORT,
+  //   user: process.env.PG_USER,
+  //   password: process.env.PG_PASSWORD,
+  //   ssl: {
+  //     rejectUnauthorized: false
+  //   }
+  // })
 
-  const client = new Client({
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  })
-
-  client.connect(err => {
-    if (err) {
-      console.error('connection error', err.stack)
-    } else {
-      console.log('connected')
-    }
-  })
+  // client.connect(err => {
+  //   if (err) {
+  //     console.error('connection error', err.stack)
+  //   } else {
+  //     console.log('connected')
+  //   }
+  // })
   
 
    // let name = args.name || 'stranger'
    // let greeting = 'Hello ' + name + '!'
     //console.log(greeting)
-    return {"body": "greeting"}
+    return {"body": "function called successfully"}
   }
-  main()
+
+  exports.main = main
   
