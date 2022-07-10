79d98475832931c5d947714f4a72982a144b1664
techswarn <tech.swarns@gmail.com>
2022-07-11 05:29:37 +0530

    code update

--- packages/databasesCon/helloPg/hello.js
+++ packages/databasesCon/helloPg/hello.js
@@ -1,48 +1,31 @@
-// // const { Client } = require('pg')
-// // const dotenv = require('dotenv')
+const { Client } = require('pg')
+const dotenv = require('dotenv')
 
-// // dotenv.config({path: './config.env'})
-
-// function main(args) {
-// // console.log(process.env)
-//   // const client = new Client({
-//   //   host: process.env.PG_HOST,
-//   //   database: process.env.PG_DATABASE,
-//   //   port: process.env.PG_PORT,
-//   //   user: process.env.PG_USER,
-//   //   password: process.env.PG_PASSWORD,
-//   //   ssl: {
-//   //     rejectUnauthorized: false
-//   //   }
-//   // })
-
-//   // client.connect(err => {
-//   //   if (err) {
-//   //     console.error('connection error', err.stack)
-//   //   } else {
-//   //     console.log('connected')
-//   //   }
-//   // })
-  
-
-//    // let name = args.name || 'stranger'
-//    // let greeting = 'Hello ' + name + '!'
-//     //console.log(greeting)
-//     return {"body": "function called successfully"}
-//   }
-
-//   exports.main = main
-// PG_DATABASE: "${PG_DATABASE}"
-// PG_PORT: "${PG_PORT}"
-// PG_USER: "${PG_USER}"
-// PG_PASSWORD: "${PG_PASSWORD}"
-// PG_HOST: "${PG_HOST}"
+dotenv.config({path: './config.env'})
 
 function main(args) {
-  let name = args.name || 'stranger'
-  let greeting = 'Hello ' + name + '!'
-  console.log(greeting)
-  return {"body": greeting}
-}
 
+  const client = new Client({
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+
+  client.connect(err => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected')
+    }
+  })
+  
+  return {"body": "function called successfully"}
+  }
+
+  main()
 exports.main = main
